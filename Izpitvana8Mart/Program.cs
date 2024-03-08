d364c33ccc31f5192b72a21ccb0127c334a0bd92
RadoKopacha <petyrnikolov0804@gmail.com>
2024-03-08 13:26:51 +0200

    imam 2

---
+++ Izpitvana8Mart/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Izpitvana8Mart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write numbers");
+            int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(' ', num));
+            Console.WriteLine(SrednoArehmetichno);
+            Console.WriteLine(BroiNanula);
+        }
+        static void SrednoArehmetichno (int[] num)
+        { 
+
+            int result = 0;
+            if (num > )
+            {
+
+            }
+        }
+        static void BroiNanula()
+        {
+         
+        }
+    }
+}
